e23f5524fc90d29fe604bf27fc89c7e3336666ba
daniellimadev <pdaniel643@gmail.com>
2024-06-06 14:45:03 -0300

    Initial commit

---
+++ src/test/java/com/github/daniellimadev/desafiotecnicodocursodejavadateamcubation/DesafioTecnicoDoCursoDeJavaDaTeamcubationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.daniellimadev.desafiotecnicodocursodejavadateamcubation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoDoCursoDeJavaDaTeamcubationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
