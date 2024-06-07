63e31bc0d8e198377e2c3b8630ebe3f9304194b5
daniellimadev <pdaniel643@gmail.com>
2024-06-07 15:13:09 -0300

    Observabilidade com Spring Boot Actuator e documentando api com swagger e criando a imagem docker da Api!

--- src/main/java/com/github/daniellimadev/desafiotecnicodocursodejavadateamcubation/controller/TransacaoController.java
+++ src/main/java/com/github/daniellimadev/desafiotecnicodocursodejavadateamcubation/controller/TransacaoController.java
@@ -3,29 +3,50 @@ package com.github.daniellimadev.desafiotecnicodocursodejavadateamcubation.contr
 import com.github.daniellimadev.desafiotecnicodocursodejavadateamcubation.dto.EstatisticasDTO;
 import com.github.daniellimadev.desafiotecnicodocursodejavadateamcubation.dto.TransacaoDTO;
 import com.github.daniellimadev.desafiotecnicodocursodejavadateamcubation.service.TransacaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(name = "api-de-estatiticas-de-transacao")
 @RequestMapping(value = "/transacao", produces = {"application/json"})
 public class TransacaoController {
     @Autowired
     private TransacaoService transacaoService;
 
+    @Operation(summary = "Cria uma transação", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transação criada com sucesso!"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválida!"),
+            @ApiResponse(responseCode = "500", description = "Serviço indisponível!"),
+    })
     @PostMapping
     public ResponseEntity<Void> salvar(@RequestBody @Valid TransacaoDTO transacaoDTO) {
         transacaoService.salvar(transacaoDTO);
         return ResponseEntity.status(201).build();
     }
 
+    @Operation(summary = "Mostra as estatísticas das transações cadastradas", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Transações deletadas com sucesso!"),
+            @ApiResponse(responseCode = "500", description = "Serviço indisponível!"),
+    })
     @GetMapping("/estatistica")
     public ResponseEntity<EstatisticasDTO> findAll() {
 
         return ResponseEntity.ok(transacaoService.estatisticas());
     }
 
+    @Operation(summary = "deleta todas as transações", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Transações deletadas com sucesso!"),
+            @ApiResponse(responseCode = "500", description = "Serviço indisponível!"),
+    })
     @DeleteMapping
     public ResponseEntity<Void> deletar() {
         transacaoService.deletar();
